576f5a22020de8d3503e7547485adbfc14c83ea1
corcor27 <cory13579@googlemail.com>
2017-06-20 17:24:00 +0100

    Update test_pycbc_inference.sh

--- test_pycbc_inference.sh
+++ test_pycbc_inference.sh
@@ -1,7 +1,5 @@
-module load ldg/pycbc-testing
 #Create new directory for this inference run
-#NAMEDIR=`date '+%Y%m%d-%H%M%S'`
-NAMEDIR=testrun
+NAMEDIR=`date '+%Y%m%d-%H%M%S'`
 mkdir ${NAMEDIR}
 PAR=${NAMEDIR}/parameters.txt
 
